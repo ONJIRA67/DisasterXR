6e387235b4baee8f76f98b0e9f584530975f4ec2
ONJIRA67 <74503894+ONJIRA67@users.noreply.github.com>
2023-02-17 08:12:48 +0700

    Update
    - create ui shop

---
+++ Assets/CashResult.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CashResult : MonoBehaviour
+{
+    int money = 100; 
+    public bool isBuyActive;
+    public bool isSellActive;
+    public int pointValue;
+    Button buy;
+    Button sell;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
